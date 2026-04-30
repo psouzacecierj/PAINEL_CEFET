85e7a55ca54a504db953fe8b0446efb30a9541c7
psouzacecierj <psouzapsouza@cecierj.edu.br>
2026-04-30 16:57:40 -0300

    Cabeçalho

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ with col_logo:
     st.image("logo-cecierj.png", width=150)
 
 with col_titulo:
-    st.title("Controle de Cadastro de Reserva – CEFET/RJ")
+    st.title("Controle de Cadastro de Reserva de Engenharia de Produção CEFET/RJ – CEDERJ")
 
 # ------------------------------------------------------
 # LEITURA DA BASE (Google Sheets - CSV)
